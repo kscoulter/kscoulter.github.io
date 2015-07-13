7d42657cfab5c4edd09161cbb9d25f31128d5d8a
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-13 09:40:48 -0400

    initial commit, playing with object properties

--- script.js
+++ script.js
@@ -1,43 +1,25 @@
-//Tic-Tac-Toe game//
-// var button = document.getElementsByTagName("button")[0];
-// var buttonText = document.getElementsByClassName("turnSwitchBtn")[0].innerHTML;
+var array = ["square", "star", "moon"];
 
-var allCells = document.getElementsByTagName("div");
-var counter = 0;
-var whoseturn = document.getElementsByClassName("whoseturn")[0]
-
-//Add click event listener to all cells
-for (var i = 0; i < allCells.length; i++){
-  allCells[i].addEventListener("click", changeValue);
+for(var i = 0; i < array.length; i++){
+  $("tr").append("<td class =" + array[i] + "></td>")
 }
 
-function changeValue(event){
-  var activeSquare = document.getElementById(event.target.id);
-  if (activeSquare.className == "cell") {
-    if(counter % 2 === 0){
-      activeSquare.innerHTML = "<p>X</p>";
-      activeSquare.classList.add("ex")
-      whoseturn.innerHTML = "Place an O"
-    } else {
-      activeSquare.innerHTML = "<p>O</p>";
-      activeSquare.classList.add("O")
-      whoseturn.innerHTML = "Place an X"
-    }
-
-    counter ++;
-  } else {
-    return;
+var cards = {
+  cardA: {
+    image: 'url("/Users/kcoulter/Documents/RicolettaCemeteryLocks/IMG_0199")',
+    class:"rustyLock",
+    matched: false
+  },
+  cardB: {
+    image: 'url("/Users/kcoulter/Documents/RicolettaCemeteryLocks/IMG_O254")',
+    class: "yale",
+    matched: false
+  },
+  cardC: {
+    image: 'url("/Users/kcoulter/Documents/RicolettaCemeteryLocks/IMG_0223")',
+    class: "rustyGreen",
+    matched: false
   }
 }
 
-var button = document.getElementsByClassName("reset")[0];
-button.addEventListener("click",resetCells);
-
-function resetCells(){
-  for (var j = 0; j < allCells.length; j++){
-    allCells[j].innerHTML = "";
-    allCells[j].setAttribute("class", "cell")
-  }
-  counter = 0;
-  whoseturn.innerHTML = "Place an X"
-}
+var keyArray = Object.keys(cards);
