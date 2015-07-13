c211d71dbed0390057c88239e093b2cade6fe6b8
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-13 09:54:58 -0400

    I can now create an array of card objects

--- script.js
+++ script.js
@@ -22,4 +22,12 @@ var cards = {
   }
 }
 
-var keyArray = Object.keys(cards);
+var cardNameArray = Object.keys(cards);
+var cardArray = [];
+
+for(var i = 0; i < cardNameArray.length; i++){
+  // console.log(cardNameArray);
+  // console.log(cardNameArray[i]);
+  // console.log(cards[cardNameArray[i]]);
+  cardArray.push(cards[cardNameArray[i]]) //how to access the card objects
+}
